614f139af7a583ac2b8f20e196f26101c9b5eebc
Ashish-Phogatjava <phogatashish06@gmail.com>
2024-05-15 08:33:17 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+import p1.TwoDim;
+import p2.ThreeDim;
+
+public class A {
+    public static void main(String args[]){
+        TwoDim ref;
+        ref = new TwoDim(1,2);
+        System.out.println(ref);
+        ref = new ThreeDim(3,4,5);
+        System.out.println(ref);
+    }
+    
+}
